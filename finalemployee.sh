de9abda868c3321b153c53dfd87aa0c6dd0e8031
priyagund <54441032+priyagund@users.noreply.github.com>
2019-11-09 17:58:12 +0530

    changes in addparttimefeature

--- finalemployee.sh
+++ finalemployee.sh
@@ -2,10 +2,10 @@
 if (( $RANDOM %2 -eq 1))
 empRatePer=10
 R=$(( $RANDOM ))
-result=$(( $R % 2 ))
+responce=$(( $R % 2 ))
 then
 echo "Employee present"
-elif [ $result -eq 0]
+elif [ $response -eq 0]
 echo "Employye parttime"
 else
 echo "employee is absent"
