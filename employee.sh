1d64df776fec3f3c08b099a062e3cebaaa1bb7ae
priyagund <priyankagund18@gmail.com>
2019-11-09 17:46:01 +0530

    add employee wage

--- employee.sh
+++ employee.sh
@@ -1,12 +1,7 @@
 #!/bin/bash -x
 if (( $RANDOM %2 -eq 1))
-empRatePer=10
-R=$(( $RANDOM ))
-result=$(( $R % 2 ))
 then
 echo "Employee present"
-elif [ $result -eq 0]
-echo "Employye parttime"
 else
 echo "employee is absent"
 fi
