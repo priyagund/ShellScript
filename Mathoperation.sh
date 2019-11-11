9db79a3bcac688d4056b677f9137b04fef003033
luckyNB <20150773@dbatu.ac.in>
2019-11-11 19:06:18 +0530

    sixth operation

--- Mathoperation.sh
+++ Mathoperation.sh
@@ -1,10 +1,18 @@
 
 #!/bin/bash -x
+
 echo enter firstNumber
 read  firstNumber
 echo enter secondNumber
 read secondNumber
 echo enter thirdNumber
 read thirdNumber
-Result=$(( ( $firstNumber % $secondNumber ) + $thirdNumber ))
-echo $Result
+declare -A operations
+
+operations[Result1]=$(( ( $firstNumber % $secondNumber ) + $thirdNumber  ))
+operations[Result2]=$(( ( $firstNumber + $secondNumber ) * $thirdNumber  ))
+operations[Result3]=$(( ( $firstNumber * $secondNumber ) + $thirdNumber  ))
+operations[Result4]=$(( ( $firstNumber + $secondNumber ) / $thirdNumber  ))
+
+echo ${operations[@]}
+
