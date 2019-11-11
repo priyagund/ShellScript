5bb03f3beabf991dd6aa05fb51f3edac57c72ef1
luckyNB <20150773@dbatu.ac.in>
2019-11-11 17:47:52 +0530

    firstoperation

--- Mathoperation.sh
+++ Mathoperation.sh
@@ -1,12 +1,10 @@
+
 #!/bin/bash -x
 echo enter firstNumber
-read firstNumber
+read  firstNumber
 echo enter secondNumber
-read seconNumber
+read secondNumber
 echo enter thirdNumber
 read thirdNumber
-
-echo firstNumber=$firstNumber
-echo secondNumber=$secondNumber
-echo thirdNumber=$thirdNumber
-
+Result=$(( ( $firstNumber + $secondNumber ) * $thirdNumber ))
+echo $Result
